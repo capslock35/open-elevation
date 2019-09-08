326cecc17997bfab7c53f792ea09ff628622b7b0
capslock35 <kaandura@gmail.com>
2019-09-08 12:20:43 +0300

    Update download-srtm-data.sh

--- download-srtm-data.sh
+++ download-srtm-data.sh
@@ -2,9 +2,9 @@
 
 set -eu
 
-wget http://gisweb.ciat.cgiar.org/TRMM/SRTM_Resampled_250m/SRTM_NE_250m_TIF.rar && \
-wget http://gisweb.ciat.cgiar.org/TRMM/SRTM_Resampled_250m/SRTM_SE_250m_TIF.rar && \
-wget http://gisweb.ciat.cgiar.org/TRMM/SRTM_Resampled_250m/SRTM_W_250m_TIF.rar && \
+wget -O SRTM_NE_250m_TIF.rar http://srtm.csi.cgiar.org/wp-content/uploads/files/250m/SRTM_NE_250m_TIF.rar && \
+wget -O SRTM_SE_250m_TIF.rar http://srtm.csi.cgiar.org/wp-content/uploads/files/250m/SRTM_SE_250m_TIF.rar && \
+wget -O SRTM_W_250m_TIF.rar http://srtm.csi.cgiar.org/wp-content/uploads/files/250m/SRTM_W_250m_TIF.rar && \
 unar -f SRTM_NE_250m_TIF.rar && \
 unar -f SRTM_SE_250m_TIF.rar && \
 unar -f SRTM_W_250m_TIF.rar
